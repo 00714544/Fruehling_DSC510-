5b446031c655b7e645917803d79e23314277838e
Deb <48359855+00714544@users.noreply.github.com>
2019-03-24 21:45:59 -0400

    Add files via upload
    Work in progress

---
+++ FtFOC.py
@@ -0,0 +1,6 @@
+#Capture user order for fiber optic cable, assignment 2.1, Deb Fruehling
+message="Welcome to Current Cable."
+company=input ('What is the name of your company?')
+print='Welcome' 'company'
+FtFOC=input ('How many feet do you need?')
+print=FtFOC*0.87
