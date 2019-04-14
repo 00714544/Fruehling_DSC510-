5e4e8e3a2339a8637cbb9ba19600c5e7a8ef1c98
Deb <48359855+00714544@users.noreply.github.com>
2019-04-13 20:36:56 -0400

    Delete FtFOC.py

--- FtFOC.py
+++
@@ -1,6 +0,0 @@
-#Capture user order for fiber optic cable, assignment 2.1, Deb Fruehling
-message="Welcome to Current Cable."
-company=input ('What is the name of your company?')
-print='Welcome' 'company'
-FtFOC=input ('How many feet do you need?')
-print=FtFOC*0.87
